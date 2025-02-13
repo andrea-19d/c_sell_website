5a6a6e4d3c543fde8cfcb7f458f8fd723ad1eee8
Andrea Robu <138387838+andrea-19d@users.noreply.github.com>
2025-02-13 21:11:26 +0200

    Removed scss files. Closes #2.

--- c_sell_website/src/components/common/counter/counter.jsx
+++ c_sell_website/src/components/common/counter/counter.jsx
@@ -1,13 +1,11 @@
-import './counter.style.scss';
-
 const Counter = () => {
     return (
-        <div className='counter-container'>
-            <div className='svg-overlay'>
+        <div className="relative w-[300px] h-[300px] bg-white mx-auto my-5 shadow-md rounded-lg overflow-hidden">
+            <div className="absolute inset-0 flex items-center justify-center pointer-events-none">
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 100 100"
-                    className="your-svg-class"
+                    className="w-[80%] h-auto opacity-50"
                 >
                 </svg>
             </div>
